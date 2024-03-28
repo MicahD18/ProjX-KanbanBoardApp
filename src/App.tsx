89841ecd18bb6d42fc0d9fdad806fd0865527879
MicahD18 <70763379+MicahD18@users.noreply.github.com>
2024-03-27 20:08:31 -0600

    Setup testing with Vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function divide(x: number, y: number) {
+  if (y === 0) {
+    throw new Error("You can't divide by zero.");
+  }
+  return Math.round(x / y);
+}
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loggedIn) {
+      setLoggedIn(true);
+      return navigate("/boards");
+    }
+  }, [loggedIn, navigate]);
+
   return (
     <div className="bg-red-500">
       <p>hello there!</p>
