97cdd111b2ab706439fee82c38a46e08600b2b0e
MicahD18 <70763379+MicahD18@users.noreply.github.com>
2024-06-11 22:21:28 -0600

    Added thunk middleware; Added tests for Header component

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import boardReducer from "./reducers/boardReducers";
 import modalReducer from "./reducers/modalReducer";
 import sidebarReducer from "./reducers/sidebarReducers";
+import { thunk } from "redux-thunk";
 
 export const rootReducer = combineReducers({
   boardReducer: boardReducer,
@@ -11,6 +12,7 @@ export const rootReducer = combineReducers({
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
