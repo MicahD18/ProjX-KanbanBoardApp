11b9e6d30aba9f930949f34f86e06c57295f5d84
MicahD18 <70763379+MicahD18@users.noreply.github.com>
2024-06-18 21:05:53 -0600

    Added all required tests for Header component; All passed

--- src/testing/components/Header.test.tsx
+++ src/testing/components/Header.test.tsx
@@ -9,7 +9,11 @@ import modalReducer from "../../reducers/modalReducer";
 import boardReducer from "../../reducers/boardReducers";
 import sidebarReducer from "../../reducers/sidebarReducers";
 import authSlice from "../../slices/authSlice";
-import { openEditBoardModal } from "../../actions/modalActions";
+import {
+  closeModal,
+  openDeleteBoardModal,
+  openEditBoardModal,
+} from "../../actions/modalActions";
 import { debug } from "console";
 
 // NEW:
@@ -59,7 +63,18 @@ describe("Header Component", () => {
     });
   });
 
-  it("should dispatch openEditBoardModal action", () => {
+  it("should render without crashing", () => {
+    const { getByRole } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    const header = getByRole("header");
+    expect(header);
+  });
+
+  it("should dispatch openEditBoardModal action on edit button click", () => {
     const { getByRole } = render(
       <Provider store={store}>
         <Header />
@@ -75,48 +90,49 @@ describe("Header Component", () => {
     expect(store.dispatch).toHaveBeenCalledWith(openEditBoardModal());
   });
 
-  // Add more tests as needed
+  it("should dispatch openDeleteBoardModal action on delete button click in EditBoardModal", () => {
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    // Open the edit board modal
+    const editButton = getByRole("button", { name: /edit board/i });
+    fireEvent.click(editButton);
+
+    // Simulate the EditBoardModal being rendered and the delete button being clicked
+    const deleteButton = getByText(/delete board/i);
+    fireEvent.click(deleteButton);
+
+    expect(store.dispatch).toHaveBeenCalledWith(openDeleteBoardModal());
+  });
+
+  it("should close the modal when handleCloseModal is called", () => {
+    store.dispatch(closeModal());
+
+    expect(store.dispatch).toHaveBeenCalledWith(closeModal());
+  });
+
+  it("should conditionally render based on location.pathname", () => {
+    // Test when pathname is "/sign-in"
+    window.location.pathname = "/sign-in";
+    const { queryByRole } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    expect(queryByRole("button", { name: /edit board/i })).not;
+
+    // Test when pathname is not "/sign-in" or "/sign-up"
+    window.location.pathname = "/";
+    const { getByRole } = render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    expect(getByRole("button", { name: /edit board/i }));
+  });
 });
-
-// OLD:
-// export const testStore = (state: Partial<RootState>) => {
-//   return configureStore({
-//     reducer: rootReducer,
-//     middleware: (getDefaultMiddleware) =>
-//       getDefaultMiddleware().concat(thunk, logger),
-//     preloadedState: state,
-//   });
-// };
-
-// export const renderWithStore = (
-//   ui: React.ReactElement,
-//   initialState: Partial<RootState>
-// ) => {
-//   function Wrapper({ children }: { children: React.ReactNode }) {
-//     return <Provider store={testStore(initialState)}>{children}</Provider>;
-//   }
-//   return render(ui, { wrapper: Wrapper });
-// };
-
-// describe("Header Component", () => {
-//   it("should render the header with the initial state", () => {
-//     const initialState = {
-//       modalReducer: { currentModal: null },
-//       boardReducer: {
-//         selectedBoard: { id: 1, name: "Test Board", columns: [] }, // Added a selectedBoard
-//         boards: [],
-//         columns: null,
-//         selectedTask: null,
-//       },
-//       sidebarReducer: { currentState: true },
-//       auth: { isAuthenticated: true },
-//     };
-
-//     const component = <Header />;
-
-//     const { getByText } = renderWithStore(component, initialState);
-
-//     expect(getByText("ProjX"));
-//     expect(getByText("Test Board"));
-//   });
-// });
